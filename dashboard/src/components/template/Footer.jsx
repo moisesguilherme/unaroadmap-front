4062b2eb9195d5a76d0e262dd8cdb06ce5796271
Moisés Guilherme <moisescomp@gmail.com>
2020-05-03 15:51:36 -0300

    dashboard

---
+++ dashboard/src/components/template/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+       <span>
+            Desenvolvido pela equipe
+            <strong> <span className="text-danger">Una</span>Roadmap</strong>
+       </span>
+    </footer>
+
+
